c74871566c53975ea88abfb10af9986704f14c9b
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-05-09 08:48:10 +0700

    feat: add something

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class DoorManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator doorAnimator;
+    public Animator doorAnimator2;
+    public Animator doorAnimator3;
+
+    public bool key1;
+    public bool key2;
+    public bool key3;
     void Start()
     {
         
@@ -14,5 +21,30 @@ public class DoorManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OpenDoor()
+    {
+        switch (gameObject.name)
+        {
+            case "Door1":
+                if (key1)
+                {
+                    doorAnimator.SetTrigger("Open");
+                }
+                break;
+            case "Door2":
+                if (key2)
+                {
+                    doorAnimator2.SetTrigger("Open");
+                }
+                break;
+            case "Door3":
+                if (key3)
+                {
+                    doorAnimator3.SetTrigger("Open");
+                }
+                break;
+        }
     }
 }
