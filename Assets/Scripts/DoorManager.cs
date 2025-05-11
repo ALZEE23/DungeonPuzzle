c1300ac0f4c014846bef8d4bf0da6941ca1bc285
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-05-11 13:14:05 +0700

    refactor: baked area

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DoorManager : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class DoorManager : MonoBehaviour
     public Animator doorAnimator;
     public Animator doorAnimator2;
     public Animator doorAnimator3;
+
+    public NavMeshObstacle doorObstacle;
+    public NavMeshObstacle doorObstacle2;
+    public NavMeshObstacle doorObstacle3;
 
     public bool key1;
     public bool key2;
@@ -31,18 +36,21 @@ public class DoorManager : MonoBehaviour
                 if (key1)
                 {
                     doorAnimator.SetTrigger("Open");
+                    doorObstacle.enabled = false;
                 }
                 break;
             case "Door2":
                 if (key2)
                 {
                     doorAnimator2.SetTrigger("Open");
+                    doorObstacle2.enabled = false;
                 }
                 break;
             case "Door3":
                 if (key3)
                 {
                     doorAnimator3.SetTrigger("Open");
+                    doorObstacle3.enabled = false;
                 }
                 break;
         }
